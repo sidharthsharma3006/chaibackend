ab402e68e10bc6b3008f55f366c8222509ce8e09
Saksham Sharma <sakshamsharma2025@gmail.com>
2024-08-09 22:38:46 +0530

    first

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express') 
+const app = express() 
+require('dotenv').config()
+const port = 3000 
+
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})  
+app.get('/twitter',(req,res)=>{
+    res.send('bye bye')
+})
+
+app.get('/login',(req,res)=>{
+    res.send('<h1>please login at chai aur code</h1>') 
+})
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Example app listening on port ${port}`) 
+}) 
